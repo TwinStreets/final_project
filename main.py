9007496a2a7b0accbcbe45d8f7ba06517953f16d
jordan white <jordanw.colt@gmail.com>
2017-07-25 09:34:52 -0700

    added a template_vars for users in MainHandler and st up new plus_one and Minus_one models

--- main.py
+++ main.py
@@ -21,10 +21,15 @@ class Artist(ndb.Model):
 
 # This is the mddle man between the user and the artist it allows them to talk to
 #  each other without being stuck to one in particular
-class Preference(ndb.Model):
+class Plus_One(ndb.Model):
     user_key = ndb.KeyProperty(kind=Post)
     artist_key = ndb.KeyProperty(kind=Post)
-    like = ndb.IntegerProperty()
+    like = ndb.BooleanProperty()
+
+class Minus_One(ndb.Model):
+    user_key = ndb.KeyProperty(kind=post)
+    artist_key = ndb.KeyProperty(kind=post)
+    dislike = ndb.BooleanProperty()
 
 # The login landing page
 class LoginHandler(webapp2.RequestHandler):
@@ -37,19 +42,34 @@ class LoginHandler(webapp2.RequestHandler):
 # page that shows artists rankings in wacky categories
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        #creating login and logout
+        current_user = users.get_current_user()
+        login_url = users.create_login_url()
+        logout_url = users.create_logout_url()
+
+        template_vars = {
+        "current_user": current_user,
+        "login_url": login_url,
+        "logout_url": logout_url,
+        }
         template = jinja_environment.get_template('templates/home.html')
-        self.response.write(template.render())
+        self.response.write(template.render(template_vars))
 
 # This is the main page where everything happens, the bulk of functionality
 class ArtistHandler(webapp2.RequestHandler):
     def get(self):
+
         template = jinja_environment.get_template('templates/artist_page.html')
         self.response.write(template.render())
 
 # This is the profile page, which is supposed to show you all the stuff in the data base about you
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
+
+
+
+
         template = jinja_environment.get_template('templates/profile.html')
         self.response.write(template.render())
 
