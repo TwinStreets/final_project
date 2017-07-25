8ddbe4ca73de8aa7bcc2847643b04b2a3b9e7ca4
jordan white <jordanw.colt@gmail.com>
2017-07-24 19:09:47 -0700

    I added a bunch of comments for each element in main.py and added a / to each handler in the dictionary

--- main.py
+++ main.py
@@ -8,37 +8,46 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+# This is the user profile
 class User(ndb.Model):
     name = ndb.StringProperty()
     blurb = ndb.StringProperty()
 
+# This is the artist profile
 class Artist(ndb.Model):
     name = ndb.StringProperty()
     bio = ndb.StringProperty()
     images = ndb.StringProperty()
 
+# This is the mddle man between the user and the artist it allows them to talk to
+#  each other without being stuck to one in particular
 class Preference(ndb.Model):
     user_key = ndb.KeyProperty(kind=Post)
     artist_key = ndb.KeyProperty(kind=Post)
     like = ndb.IntegerProperty()
 
+# The login landing page
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
         template = jinja_environment.get_template('templates/log_in_page.html')
         self.response.write(template.render())
 
+# We can start this with being a simple about page then change it to be a dinamic
+# page that shows artists rankings in wacky categories
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
         template = jinja_environment.get_template('templates/home.html')
         self.response.write(template.render())
 
+# This is the main page where everything happens, the bulk of functionality
 class ArtistHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/artist_page.html')
         self.response.write(template.render())
 
+# This is the profile page, which is supposed to show you all the stuff in the data base about you
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/profile.html')
@@ -47,7 +56,7 @@ class ProfileHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('login', LoginHandler),
-    ('artist', ArtistHandler),
-    ('profile',ProfileHandler)
+    ('/login', LoginHandler),
+    ('/artist', ArtistHandler),
+    ('/profile',ProfileHandler)
 ], debug=True)
