d2bfa8b445c19d1b937289d3fb36dd0e66731b4b
Kayla Vokt <kaylavokt@gmail.com>
2017-07-27 16:41:03 -0700

    real version 4

--- main.py
+++ main.py
@@ -34,6 +34,14 @@ class Likes(ndb.Model):
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+        artist = [ Artist(name='Drake',genre='hip hop',image='https://i.scdn.co/image/cb080366dc8af1fe4dc90c4b9959794794884c66'), Artist(name='John Mayer', genre='neo mellow', image='https://i.scdn.co/image/96a2e527431f7bf39cea4bf8702fc8159f08e2aa'), Artist(name='Logic',genre='rap',image='https://i.scdn.co/image/9aab47129b8405aa80afc5590ed295b7899154f1') ]
+
+        artist_query = Artist.query().fetch()
+
+        if not artist_query:
+            for a in artist:
+                a.put()
+
         artist_query = Artist.query()
         artists = artist_query.fetch()
 
@@ -70,13 +78,6 @@ class MainHandler(webapp2.RequestHandler):
 class ArtistHandler(webapp2.RequestHandler):
     def get(self):
 
-        artist = [ Artist(name='Drake',genre='hip hop',image='https://i.scdn.co/image/cb080366dc8af1fe4dc90c4b9959794794884c66'), Artist(name='John Mayer', genre='neo mellow', image='https://i.scdn.co/image/96a2e527431f7bf39cea4bf8702fc8159f08e2aa'), Artist(name='Logic',genre='rap',image='https://i.scdn.co/image/9aab47129b8405aa80afc5590ed295b7899154f1') ]
-
-        artist_query = Artist.query()
-
-        if not artist_query:
-            for a in artist:
-                a.put()
 
         urlsafe_key2 = self.request.get('key')
         artist_key = ndb.Key(urlsafe=urlsafe_key2)
